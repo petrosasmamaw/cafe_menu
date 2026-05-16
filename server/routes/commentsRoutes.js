01e603997ae6a9cd1e3d4392d9433746650a537c
petrosasmamaw <petrosasmamaw2@gmail.com>
2026-05-16 22:21:32 +0300

    Add comments API and fix duplicate router import

--- server/routes/commentsRoutes.js
+++ server/routes/commentsRoutes.js
@@ -7,12 +7,3 @@ router.get('/', listComments)
 router.post('/', createComment)
 
 export default router
-import express from 'express'
-import { createComment, listComments } from '../controllers/commentsController.js'
-
-const router = express.Router()
-
-router.post('/', createComment)
-router.get('/', listComments)
-
-export default router
